f687813e615f3588af09e43d3e0429dd7b74f4df
shalbuz <sh.mursalov@mail.ru>
2025-10-29 12:23:46 +0300

    post children

--- src/components/Profile/Post/PostItem.jsx
+++ src/components/Profile/Post/PostItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import cl from "./PostItem.module.css"
+import s from "./PostItem.module.css"
 
-const PostItem = () => {
+const PostItem = (props) => {
     return (
-        <div className={ cl.item }>
-            <div className={ cl.avatar }><img src="https://placehold.co/52x52/orange/white.png" alt=""/></div>
-            <p>Hey, why nobody love me?</p>
+        <div className={ s.item }>
+            <div className={ s.avatar }><img src="https://placehold.co/52x52/orange/white.png" alt=""/></div>
+            <p>{ props.children }</p>
         </div>
     )
 }
