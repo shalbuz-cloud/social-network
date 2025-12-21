c18ce74669f7657fcf3aa2fa17acc443afca4a75
shalbuz <sh.mursalov@mail.ru>
2025-12-21 19:52:10 +0300

    feat: componentDidUpdate

--- src/components/Profile/ProfileStatus.jsx
+++ src/components/Profile/ProfileStatus.jsx
@@ -19,6 +19,12 @@ class ProfileStatus extends React.Component {
         this.setState({status: e.target.value});
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status});
+        }
+    }
+
     render() {
         return (
             <div>
